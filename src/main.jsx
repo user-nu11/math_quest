ffc8127722cf39e9ae0cb57a27b52dcca712369e
Zuzanna Hauzer <156180707+user-nu11@users.noreply.github.com>
2025-06-11 18:50:25 +0200

    login/sign in script

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,78 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+document.addEventListener("DOMContentLoaded", function() {
+  var create = document.getElementById("linkCreateAccount");
+  var haveAcc = document.getElementById("linkLogin");
+  var formLogin = document.getElementById("Login");
+  var formCreateAcc = document.getElementById("CreateAccount");
+  var loginButton = document.getElementById("loginButton");
+  var regButton = document.getElementById("registerButton");
+
+  create.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    hide(formLogin);
+    show(formCreateAcc);
+  });
+
+  haveAcc.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    show(formLogin);
+    hide(formCreateAcc);
+  });
+
+  loginButton.addEventListener("click", function(e) {
+
+   $('#check').on("click", function(){
+  let valid = true;
+  $('[required]').each(function() {
+    if ($(this).is(':invalid') || !$(this).val()) valid = false;
+  })
+  if (!valid) {
+    alert("error please fill all fields!");
+  } 
+  if (valid) {
+    alert('valid');
+    hide(formLogin);
+    hide(formCreateAcc);
+    show.getElementById("game");
+  }
+
+  })
+  e.preventDefault();
+  });
+
+  regButton.addEventListener("click", function(e) {
+
+    $('#check').on("click", function(){
+    let valid = true;
+    $('[required]').each(function() {
+     if ($(this).is(':invalid') || !$(this).val()) valid = false;
+    })
+    if (!valid) {
+      alert("error please fill all fields!");
+    } 
+    if(valid) {
+      alert('valid');
+      show(formLogin);
+      hide(formCreateAcc);
+    }
+   })
+  e.preventDefault();
+  });
+
+
+});
+
+function hide(elem) {
+    elem.classList.add("form--hidden");
+    elem.classList.remove("form--unhidden");
+}
+
+function show(elem) {
+  elem.classList.add("form--unhidden");
+  elem.classList.remove("form--hidden");
+}
